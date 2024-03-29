b5d7d7622042f569b38d4372303223d514912457
Michal Drobňák <mi.drobnak@gmail.com>
2024-03-29 17:59:54 +0100

    feat: add list view

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,15 @@
 <script lang="ts">
   import Calendar from './lib/components/calendar/Calendar.svelte';
+  import List from './lib/components/list/List.svelte';
+
+  const urlParams = new URLSearchParams(window.location.search);
+  const viewQuery = urlParams.get('view');
 </script>
 
 <main class="h-dvh w-dvw">
-  <Calendar />
+  {#if viewQuery === 'list'}
+    <List />
+  {:else}
+    <Calendar />
+  {/if}
 </main>
