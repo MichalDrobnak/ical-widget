2a91013977b38dacbff927e1f829d04d3bf2f065
Michal Drobňák <mi.drobnak@gmail.com>
2024-03-29 18:21:30 +0100

    feat: list data fetching logic

--- src/lib/components/list/List.svelte
+++ src/lib/components/list/List.svelte
@@ -1,8 +1,41 @@
 <script lang="ts">
-  import type { IRoom } from '../../models/interfaces';
+  import { REFETCH_DATA_INTERVAL_MS } from '../../constants';
+  import type { IRoom, IRoomReservations } from '../../models/interfaces';
+  import { fetchRoom } from '../../utils';
 
   export let currentDate: Date;
   export let room: IRoom;
+
+  let loading = true;
+  let error = false;
+  let reservations: IRoomReservations;
+
+  const fetchReservations = async (): Promise<void> => {
+    reservations = await fetchRoom(room!);
+  };
+
+  const fetchData = async (showLoading: boolean) => {
+    showLoading && (loading = true);
+
+    try {
+      await fetchReservations();
+      error = false;
+    } catch (e) {
+      error = true;
+    } finally {
+      loading = false;
+    }
+  };
+
+  const main = () => {
+    // Initial data fetch
+    fetchData(true);
+
+    // Refetch data every 5 minutes
+    setInterval(async () => fetchData(false), REFETCH_DATA_INTERVAL_MS);
+  };
+
+  main();
 </script>
 
 <p>{currentDate} - {room.name}</p>
